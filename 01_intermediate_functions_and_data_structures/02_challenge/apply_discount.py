887e014a21c3adcf98bf35833df5e320d45c4f80
momoko <momoko.yamada@woven.toyota>
2024-05-17 14:57:37 +0900

    Submit for granding

--- 01_intermediate_functions_and_data_structures/02_challenge/apply_discount.py
+++ 01_intermediate_functions_and_data_structures/02_challenge/apply_discount.py
@@ -1,2 +1,31 @@
-def apply_discount(products, minimum_price, discount_rate):
-    pass
+def apply_discount(
+    products, minimum_price, discount_rate
+):
+   calculate_discount = lambda a : a * (1-discount_rate*0.01)
+   
+   
+   result = [
+       (
+           elem["name"],
+           calculate_discount(elem["price"]),
+       )
+       for elem in products
+       if elem["price"] >= minimum_price
+   ]
+   print(result)
+   return result
+   
+data1=[
+    {"name": "Laptop", "category": "Electronics", "price": 1200},
+    {"name": "Bread", "category": "Food", "price": 2},
+    {"name": "Jacket", "category": "Apparel", "price": 100}
+]
+
+data2=[
+    {"name": "Smartphone", "category": "Electronics", "price": 800},
+    {"name": "Sneakers", "category": "Footwear", "price": 120},
+    {"name": "Coffee", "category": "Beverage", "price": 5}
+]
+
+apply_discount(data1,50,10)
+apply_discount(data2,100,15)
