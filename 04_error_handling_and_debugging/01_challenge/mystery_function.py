64f372f7f54139f39344fdc350108974dd6704bc
momoko <momoko.yamada@woven.toyota>
2024-06-07 07:07:55 +0900

    Submit for granding

--- 04_error_handling_and_debugging/01_challenge/mystery_function.py
+++ 04_error_handling_and_debugging/01_challenge/mystery_function.py
@@ -1,6 +1,15 @@
 def mystery_function(lst):
-    result = lst
-    for i in range(len(lst)):
+    result = []
+    for i in lst:
         if i % 2 == 0:
-            result[i] = i ** 2
+            result.append(i ** 2)
+        else:
+            result.append(i)
+    print(result)
     return result
+
+
+if __name__ == "__main__" :
+    list1= [1, 2, 3, 4, 5]
+    list2= [4, 1, 6, 2, 10]
+    mystery_function(list1)
